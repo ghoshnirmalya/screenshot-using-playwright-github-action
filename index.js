dbef3307714152b5ca58b3943a8f1116008b7ee4
Nirmalya Ghosh <nirmalya.email@gmail.com>
2020-06-22 09:31:36 +0530

    feat: uses ImageKit instead of Imgur

--- index.js
+++ index.js
@@ -16,23 +16,21 @@ async function run(name, args, url) {
   const image = buffer.toString("base64");
 
   const formdata = new FormData();
-  formdata.append("image", image);
-  formdata.append("type", "base64");
-  formdata.append("title", `${url}-${name}`);
-  formdata.append("description", `Screenshot of ${url} using ${name} browser`);
+  formdata.append("file", image);
+  formdata.append("fileName", `${url}-${name}`);
 
   const requestOptions = {
     method: "POST",
     headers: {
-      Authorization: `Client-ID 244c7118193f2e4 ${process.env.IMGUR_CLIENT_ID}`,
+      Authorization: `Basic ${process.env.IMAGEKIT_PRIVATE_KEY}`,
     },
     body: formdata,
     redirect: "follow",
   };
 
-  fetch("https://api.imgur.com/3/image", requestOptions)
+  fetch("https://upload.imagekit.io/api/v1/files/upload", requestOptions)
     .then((response) => response.json())
-    .then((result) => console.log(result.data.link))
+    .then((result) => console.log(result))
     .catch((error) => console.log("error", error));
 
   await browser.close();
