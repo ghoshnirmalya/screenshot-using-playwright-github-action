e5de5bb125ceeb28ac15c5431e8b68874663b4e7
Nirmalya Ghosh <nirmalya.email@gmail.com>
2020-06-22 23:19:31 +0530

    feat: converts to TypeScript

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+require('dotenv').config()
+
+const launchPlaywright = require('./lib/launch-playwright')
+;(async () => {
+  await launchPlaywright(
+    'chromium',
+    ['--no-sandbox'],
+    'http://whatsmyuseragent.org/'
+  )
+  await launchPlaywright('webkit', [], 'http://whatsmyuseragent.org/')
+  await launchPlaywright('firefox', [], 'http://whatsmyuseragent.org/')
+})()
---
+++ src/lib/send-data-to-image-kit.ts
@@ -0,0 +1,37 @@
+const FormData = require("form-data")
+const fetch = require("node-fetch")
+const sendDataToFaunaDB = require("./send-data-to-fauna-db")
+
+if (!globalThis.fetch) {
+  globalThis.fetch = fetch
+}
+
+const sendDataToImageKit = async (image: string, url: string, name: string) => {
+  const formdata = new FormData()
+
+  formdata.append("file", image)
+  formdata.append("fileName", `${url}-${name}`)
+
+  const requestOptions = {
+    method: "POST",
+    headers: {
+      Authorization: `Basic ${process.env.IMAGEKIT_PRIVATE_KEY}`,
+    },
+    body: formdata,
+    redirect: "follow",
+  }
+
+  try {
+    const response = await fetch(
+      "https://upload.imagekit.io/api/v1/files/upload",
+      requestOptions
+    )
+    const data = await response.json()
+
+    sendDataToFaunaDB(data)
+  } catch (error) {
+    console.log("error", error)
+  }
+}
+
+export default sendDataToImageKit
